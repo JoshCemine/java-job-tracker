2175efb2e216a7d7e61b14bca167f9e02392ffb6
JoshCemine <j.jcemine@gmail.com>
2026-05-23 08:29:31 -0700

    Init Job Tracker Java Spring Project

---
+++ src/main/java/work/smols/javajobtracker/JavaJobTrackerApplication.java
@@ -0,0 +1,13 @@
+package work.smols.javajobtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJobTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaJobTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/work/smols/javajobtracker/JavaJobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package work.smols.javajobtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJobTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
